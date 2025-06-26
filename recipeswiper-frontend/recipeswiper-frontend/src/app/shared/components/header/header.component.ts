f2e2f13319dc0f7a94b4a32f5a6a16b1366b0f88
JanisHuber <janis.huber@itme.ch>
2025-06-26 13:56:49 +0200

    --

--- recipeswiper-frontend/recipeswiper-frontend/src/app/shared/components/header/header.component.ts
+++ recipeswiper-frontend/recipeswiper-frontend/src/app/shared/components/header/header.component.ts
@@ -6,11 +6,10 @@ import {
   ElementRef,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UserService } from '../../../core/services/user.service';
-import { User } from '../../../core/models/dto/User';
+import { UserService } from '@core/services/user.service';
+import { User } from '@core/models/dto/User';
 import { Router } from '@angular/router';
-import { Group } from '../../../core/models/dto/Group';
-
+import { Group } from '@core/models/dto/Group';
 
 @Component({
   selector: 'app-header',
